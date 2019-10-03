01dd68f07e675b1b6c8ff67356f7a09fdd5c9e49
Jahyun Oh <soharu1024@gmail.com>
2019-10-03 22:55:39 +0900

    Complete "Handle User Input" tutorial

---
+++ Landmarks/Landmarks/Models/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Jahyun Oh on 03/10/2019.
+//  Copyright © 2019 Jahyun Oh. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
