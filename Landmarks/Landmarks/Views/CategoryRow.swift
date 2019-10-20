16b7e606d6e6c82e3229c67d92588fbbbb6e97bf
Jahyun Oh <soharu1024@gmail.com>
2019-10-20 22:27:08 +0900

    Complete "Compose the Home View" section

--- Landmarks/Landmarks/Views/CategoryRow.swift
+++ Landmarks/Landmarks/Views/CategoryRow.swift
@@ -11,23 +11,39 @@ import SwiftUI
 struct CategoryRow: View {
     var categoryName: String
     var items: [Landmark]
-    
+
     var body: some View {
         VStack(alignment: .leading) {
             Text(self.categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
                 .padding(.top, 5)
-            
+
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(self.items) { landmark in
-                        Text(landmark.name)
+                        CategoryItem(landmark: landmark)
                     }
                 }
             }
             .frame(height: 185)
         }
+    }
+}
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
     }
 }
 
