c9f3277e80e5357744197fb674a5a2fd3bb971a5
Jahyun Oh <soharu1024@gmail.com>
2019-10-22 00:01:37 +0900

    Complete "Delay Edit Propagation" section

--- Landmarks/Landmarks/Views/ProfileHost.swift
+++ Landmarks/Landmarks/Views/ProfileHost.swift
@@ -16,13 +16,28 @@ struct ProfileHost: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                if self.mode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        self.draftProfile = self.userData.profile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }
+
+                }
+
                 Spacer()
+
                 EditButton()
             }
             if self.mode?.wrappedValue == .inactive {
                 ProfileSummary(profile: draftProfile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        self.draftProfile = self.userData.profile
+                    }
+                    .onDisappear {
+                        self.userData.profile = self.draftProfile
+                    }
             }
         }
         .padding()
